17ed3e25a45e1d4ff31c20af10c795685a8cab58
Jeremy <jeremymcote@gmail.com>
2024-09-28 11:59:29 -0400

    Fixup database

--- composeApp/src/commonMain/kotlin/com/register_renegades/totem/cache/Database.kt
+++ composeApp/src/commonMain/kotlin/com/register_renegades/totem/cache/Database.kt
@@ -1,17 +1,19 @@
 package com.register_renegades.totem.cache
 
-internal class Database(databaseDriverFactory: DatabaseDriverFactory) {
-    private val database = AppDatabase(databaseDriverFactory.createDriver())
-    private val dbQuery = database.appDatabaseQueries
+import com.register_renegades.totem.entity.File
 
-    internal fun getAllFiles(){
-        return dbQuery.selectAllFiles(::mapFileSelecting).executeAsLlist()
+internal class Database(databaseDriverFactory: DatabaseDriverFactory) {
+    private val database = TotemDatabase(databaseDriverFactory.createDriver())
+    private val dbQuery = database.totemDatabaseQueries
+
+    internal fun getAllFiles(): List<File> {
+        return dbQuery.selectAllFiles(::mapFileSelecting).executeAsList()
     }
 
-    private fun mapFileSelecting(id: Int, name: String): File{
+    private fun mapFileSelecting(id: Long, name: String?): File {
         return File(
             id = id.toInt(),
-            name = name
+            name = name ?: "Name not found"
         )
     }
 
--- composeApp/src/commonMain/kotlin/com/register_renegades/totem/cache/DatabaseDriverFactory.kt
+++ composeApp/src/commonMain/kotlin/com/register_renegades/totem/cache/DatabaseDriverFactory.kt
@@ -1,9 +1,7 @@
-//package com.register_renegades.totem.com.register_renegades.totem.cache
-//
-//import app.cash.sqldelight.db.SqlDriver
-//
-//interface DatabaseDriverFactory {
-//    interface DatabaseDriverFactory {
-//        fun createDriver(): SqlDriver
-//    }
-//}
+package com.register_renegades.totem.cache
+
+import app.cash.sqldelight.db.SqlDriver
+
+interface DatabaseDriverFactory {
+    fun createDriver(): SqlDriver
+}
