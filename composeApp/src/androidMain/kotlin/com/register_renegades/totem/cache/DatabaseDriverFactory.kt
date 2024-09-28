4677548f77a9eab6c5a9adb8045058ab29eabd42
Jeremy <jeremymcote@gmail.com>
2024-09-28 15:28:41 -0400

    Fix AndroidDatabaseDriverFactory

--- composeApp/src/androidMain/kotlin/com/register_renegades/totem/cache/DatabaseDriverFactory.kt
+++ composeApp/src/androidMain/kotlin/com/register_renegades/totem/cache/DatabaseDriverFactory.kt
@@ -5,10 +5,8 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import com.register_renegades.totem.cache.TotemDatabase
 
-interface DatabaseDriverFactory {
-    class AndroidDatabaseDriverFactory(private val context: Context) : DatabaseDriverFactory {
-        fun createDriver(): SqlDriver {
-            return AndroidSqliteDriver(TotemDatabase.Schema, context, "launch.db")
-        }
+class AndroidDatabaseDriverFactory(private val context: Context) : DatabaseDriverFactory {
+    override fun createDriver(): SqlDriver {
+        return AndroidSqliteDriver(TotemDatabase.Schema, context, "launch.db")
     }
 }
