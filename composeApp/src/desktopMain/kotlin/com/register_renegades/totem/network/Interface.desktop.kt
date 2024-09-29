3bf284653f1c47e87b925c1c329e3f132e2625d1
louis <louisphilstarno@hotmail.ca>
2024-09-29 07:00:28 -0400

    Build successful. Fixed interface issues.

--- composeApp/src/desktopMain/kotlin/com/register_renegades/totem/network/Interface.desktop.kt
+++
@@ -1,7 +0,0 @@
-package com.register_renegades.totem.network
-
-actual class InterfaceManager actual constructor(system: SystemInterfaceManager) {
-    actual fun getInterface(): Interface {
-        TODO("Not yet implemented")
-    }
-}
